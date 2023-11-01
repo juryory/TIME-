bc904f410d4c1e626369b4cbd9f3f7f0f2a3b7e4
juryory <39689527+juryory@users.noreply.github.com>
2023-11-01 20:43:53 +0800

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+const app = getApp()
+
+Page({
+  data: {},
+  navigateToLink: function() {
+    // 这里添加您的导航逻辑或调用API
+    // 例如：
+    tt.navigateTo({
+      url: 'https://ahvwy.xet.tech/s/19ynNa'
+    });
+  }
+});
